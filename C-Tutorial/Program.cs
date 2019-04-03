6118b8904c820b450f7e6d6794e7fe0b5c01a27f
phạm anh tuấn <anhtuanst1@gmail.com>
2019-04-03 17:56:59 +0700

    Learn to if...else

--- C-Tutorial/Program.cs
+++ C-Tutorial/Program.cs
@@ -6,7 +6,92 @@ namespace C_Tutorial
     {
         static void Main(string[] args)
         {
+            //======================== Structure Basic ========================
+
+            /* chuong trinh in dong chu Hello World trong C# */
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Chuong trinh minh hoa tinh huong doi tuong trong C#");
+            Console.WriteLine("--------------------------------------------------\n");
+            //tao doi tuong Rectangle
+            Rectangle r = new Rectangle();
+            //goi cac phuong thuc cua doi tuong nay
+            r.Acceptdetails();
+            r.Display();
+
+            //======================== Structure Basic ========================
+
+            /* Kieu du lieu */
+            object obj;
+            obj = 100;
+
+            dynamic dem = 20;
+
+            String str = "Hi, I am Tuan";
+
+            Console.WriteLine("\n Kieu object: "+ obj +"\n Kieu dynamic: "+ dem +"\n Kieu string: "+ str +"\n");
+
+            /* Ep kieu */
+            int x = 75;
+            float y = 53.005f;
+            double z = 2345.7652;
+            bool check = true;
+            //su dung phuong thuc ToString()
+            Console.WriteLine(x + "\t\t to string \t" + x.ToString());
+            Console.WriteLine(y + "\t\t to string \t" + y.ToString());
+            Console.WriteLine(z + "\t to int \t" + (int)z);
+            Console.WriteLine(check + "\t\t to string \t" + check.ToString());
+
+            //khai bao bien
+            Console.WriteLine("\nKhai bao va khoi tao bien trong C#");
+            short a;
+            int b;
+            double c;
+            int d = 3, e = 5;
+
+            /* khoi tao bien */
+            a = 10;
+            b = 20;
+            c = a + b;
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            Console.WriteLine("d = {0}, e = {1}", d, e);
+
+            /* Nhap gia tri */
+            String giatri;
+            Console.Write("\n Input gia tri: ");
+            giatri = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n Gia tri la: {0}", giatri);
+
+            //Hang
+            Console.WriteLine("Hang so trong C#");
+            Console.WriteLine("-------------------");   
+
+            //khai bao hang so pi
+            const double pi = 3.14159;
+
+            double bk;
+            Console.Write("Nhap ban kinh: ");
+            bk = Convert.ToDouble(Console.ReadLine());
+            double dien_tich = pi * bk * bk;
+            Console.WriteLine("\nBan kinh: {0}, Dien tich: {1}", bk, dien_tich);
+
+
+
+
+
+
+
+            //=================================================================
+            //=================================================================
+
+            Console.ReadLine();
+            /* ngan can chuong trinh run, close 1 cach nhanh chong cho den khi push 1 char */
+            //Console.ReadKey();
+
+            if (Console.ReadKey().Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
         }
     }
 }
